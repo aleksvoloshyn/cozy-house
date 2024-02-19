30b7e5b6a8213b36db5f903877461610264f7f15
Alexei Voloshyn <75684845+aleksvoloshyn@users.noreply.github.com>
2024-02-19 13:41:54 +0200

    styles added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
-console.log('hi app')
+// console.log('hi app')
+document.addEventListener('DOMContentLoaded', function () {
+  const navItems = document.querySelectorAll('.start-screen__nav-item')
+  let activeItem = null
+
+  navItems.forEach(function (item) {
+    item.addEventListener('mouseenter', function () {
+      if (activeItem !== null) {
+        activeItem.classList.remove('active')
+      }
+      item.classList.add('active')
+      activeItem = item
+    })
+
+    item.addEventListener('click', function (event) {
+      if (activeItem !== null) {
+        activeItem.classList.remove('active')
+      }
+      item.classList.add('active')
+      activeItem = item
+      event.stopPropagation()
+    })
+  })
+
+  document
+    .querySelector('.start-screen__nav')
+    .addEventListener('mouseleave', function () {
+      navItems.forEach(function (item) {
+        item.classList.remove('active')
+      })
+      activeItem = null
+    })
+})
